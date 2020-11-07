47d88e97f4af49c25cc07d0d0a8b54d380401ee1
Parv Garg <parv3213@gmail.com>
2020-11-07 23:58:54 +0530

    update on network change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,78 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header.jsx";
+import AddFlight from "./AddFlight.jsx";
 import Web3 from "web3";
 
-const {REACT_APP_NETWORK_ID} = process.env;
+const { REACT_APP_NETWORK_ID } = process.env;
 
 export default function App() {
-  const [web3, setWeb3] = useState(Object);
+  const [web3, setWeb3] = useState(undefined);
   const [account, setAccount] = useState("");
   const [accountBalance, setAccountBalance] = useState("");
   const [networkId, setNetworkId] = useState(0);
+  const [metamaskChange, setMetaMaskChange] = useState(true);
+
+  // ! make a separate file
+  const getWeb3 = () => {
+    return new Promise(async (resolve, reject) => {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          await window.ethereum.enable();
+          resolve(web3);
+        } catch (e) {
+          reject(e);
+        }
+      } else if (window.web3) {
+        resolve(window.web3);
+      } else {
+        window.alert("Must install Metamask Extension!\nDApp will not load");
+        reject("Must install Metamask Extension!");
+      }
+    });
+  };
 
   useEffect(() => {
-    const init = async() => {
-      const web3 = new Web3(Web3.givenProvider) 
+    const init = async () => {
+      const web3 = await getWeb3();
       const account = (await web3.eth.getAccounts())[0];
       const networkId = await web3.eth.net.getId();
-      if (networkId !== parseInt(REACT_APP_NETWORK_ID)){
-        console.log("Not correct",networkId,REACT_APP_NETWORK_ID ); //! render something here
+      if (networkId !== parseInt(REACT_APP_NETWORK_ID)) {
+        console.log("Not correct", networkId, REACT_APP_NETWORK_ID); //! render something here
       }
-      const accountBalance = Math.floor(parseFloat(web3.utils.fromWei(await web3.eth.getBalance(account)))*100)/100
+      const accountBalance = Math.floor(parseFloat(web3.utils.fromWei(await web3.eth.getBalance(account))) * 100) / 100
       setWeb3(web3)
       setAccount(account)
       setNetworkId(networkId)
       setAccountBalance(accountBalance);
     }
     init();
-  }, []) //! check functionality
+  }, [metamaskChange])
+
+  useEffect(() => {
+      window.ethereum.on("accountsChanged", () => {
+        console.log("Account changed");
+        setMetaMaskChange(m => !m)
+      })
+      window.ethereum.on("networkChanged", () => {
+        console.log("Network changed");
+        setMetaMaskChange(m => !m)
+      })
+  }, [])
 
   return (
     <div>
-      <Header 
-        networkId = {networkId}
-        account = {account}
-        accountBalance = {accountBalance}
+      <Header
+        networkId={networkId}
+        account={account}
+        accountBalance={accountBalance}
       />
-      <div>Hello</div>
+      <div className="container">
+        <AddFlight
+          web3={web3}
+          account={account}
+        />
+      </div>
     </div>
   );
 }
