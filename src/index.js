9664246dfe9afc4491260ddbc33588592522005d
Parv Garg <parv3213@gmail.com>
2020-11-07 15:00:33 +0530

    added flight png

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './components/App.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
