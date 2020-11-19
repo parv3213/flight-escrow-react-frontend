2dc5e4d716218d92ee1d5abd786b31a7742b798a
Parv Garg <parv@somish.com>
2020-11-19 14:27:08 +0530

    added routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header.jsx";
 import AddFlight from "./AddFlight.jsx";
 import AvailableFlight from "./AvailableFlight.jsx";
@@ -7,79 +8,74 @@ import Web3 from "web3";
 const { REACT_APP_NETWORK_ID } = process.env;
 
 export default function App() {
-  const [web3, setWeb3] = useState(undefined);
-  const [account, setAccount] = useState("");
-  const [accountBalance, setAccountBalance] = useState("");
-  const [networkId, setNetworkId] = useState(0);
-  const [metamaskChange, setMetaMaskChange] = useState(true);
-  const [refresh, setRefresh] = useState(true);
+    const [web3, setWeb3] = useState(undefined);
+    const [account, setAccount] = useState("");
+    const [accountBalance, setAccountBalance] = useState("");
+    const [networkId, setNetworkId] = useState(0);
+    const [metamaskChange, setMetaMaskChange] = useState(true);
+    const [refresh, setRefresh] = useState(true);
 
-  // ! make a separate file
-  const getWeb3 = () => {
-    return new Promise(async (resolve, reject) => {
-      if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-          await window.ethereum.enable();
-          resolve(web3);
-        } catch (e) {
-          reject(e);
-        }
-      } else if (window.web3) {
-        resolve(window.web3);
-      } else {
-        window.alert("Must install Metamask Extension!\nDApp will not load");
-        reject("Must install Metamask Extension!");
-      }
-    });
-  };
+    // ! make a separate file
+    const getWeb3 = () => {
+        return new Promise(async (resolve, reject) => {
+            if (window.ethereum) {
+                const web3 = new Web3(window.ethereum);
+                try {
+                    await window.ethereum.enable();
+                    resolve(web3);
+                } catch (e) {
+                    reject(e);
+                }
+            } else if (window.web3) {
+                resolve(window.web3);
+            } else {
+                window.alert("Must install Metamask Extension!\nDApp will not load");
+                reject("Must install Metamask Extension!");
+            }
+        });
+    };
 
-  useEffect(() => {
-    const init = async () => {
-      const web3 = await getWeb3();
-      const account = (await web3.eth.getAccounts())[0];
-      const networkId = await web3.eth.net.getId();
-      if (networkId !== parseInt(REACT_APP_NETWORK_ID)) {
-        console.log("Not correct", networkId, REACT_APP_NETWORK_ID); //! render something here
-      }
-      const accountBalance = Math.floor(parseFloat(web3.utils.fromWei(await web3.eth.getBalance(account))) * 100) / 100
-      setWeb3(web3)
-      setAccount(account)
-      setNetworkId(networkId)
-      setAccountBalance(accountBalance);
-    }
-    init();
-  }, [metamaskChange, refresh])
+    useEffect(() => {
+        const init = async () => {
+            const web3 = await getWeb3();
+            const account = (await web3.eth.getAccounts())[0];
+            const networkId = await web3.eth.net.getId();
+            if (networkId !== parseInt(REACT_APP_NETWORK_ID)) {
+                console.log("Not correct", networkId, REACT_APP_NETWORK_ID); //! render something here
+            }
+            const accountBalance = Math.floor(parseFloat(web3.utils.fromWei(await web3.eth.getBalance(account))) * 100) / 100;
+            setWeb3(web3);
+            setAccount(account);
+            setNetworkId(networkId);
+            setAccountBalance(accountBalance);
+        };
+        init();
+    }, [metamaskChange, refresh]);
 
-  useEffect(() => {
-      window.ethereum.on("accountsChanged", () => {
-        console.warn("Account changed");
-        setMetaMaskChange(m => !m)
-      })
-      window.ethereum.on("networkChanged", () => {
-        console.warn("Network changed");
-        setMetaMaskChange(m => !m)
-      })
-  }, [])
+    useEffect(() => {
+        window.ethereum.on("accountsChanged", () => {
+            console.warn("Account changed");
+            setMetaMaskChange((m) => !m);
+        });
+        window.ethereum.on("networkChanged", () => {
+            console.warn("Network changed");
+            setMetaMaskChange((m) => !m);
+        });
+    }, []);
 
-  return (
-    <div>
-      <Header
-        networkId={networkId}
-        account={account}
-        accountBalance={accountBalance}
-      />
-      <div className="container">
-        <AddFlight
-          web3={web3}
-          account={account}
-          refresh={refresh}
-          setRefresh={setRefresh}
-        />
-        <AvailableFlight 
-          web3={web3}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <Header networkId={networkId} account={account} accountBalance={accountBalance} />
+            <div className="container">
+                <Switch>
+                    <Route path="/add">
+                        <AddFlight web3={web3} account={account} refresh={refresh} setRefresh={setRefresh} />
+                    </Route>
+                    <Route path="/flights">
+                        <AvailableFlight web3={web3} />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import Modal from 'react-bootstrap/Modal'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
 import logo from "../public/plane.png";
 
 export default function Header(props) {
-    const [networkName, setNetworkName] = useState("Undefined Network")
-    const [showAddress, setShowAddress] = useState(false)
+    const [networkName, setNetworkName] = useState("Undefined Network");
+    const [showAddress, setShowAddress] = useState(false);
     const handleClose = () => setShowAddress(false);
     const handleShow = () => setShowAddress(true);
 
     useEffect(() => {
         const findNetworkName = () => {
-            if (props.networkId === 42){
-                setNetworkName("Kovan")
-            } else if (props.networkId === 1){
-                setNetworkName("Mainnet")
-            } else if (props.networkId === 5777){
-                setNetworkName("Local Test")
+            if (props.networkId === 42) {
+                setNetworkName("Kovan");
+            } else if (props.networkId === 1) {
+                setNetworkName("Mainnet");
+            } else if (props.networkId === 5777) {
+                setNetworkName("Local Test");
             }
-        }
+        };
         findNetworkName();
-    }, [props.networkId, props.accountBalance])
+    }, [props.networkId, props.accountBalance]);
     return (
         <div id="header">
             <nav className="navbar navbar-expand-lg my-3">
-                <img src={logo} alt="Airplane" style={{ width: "25px", heigh: "25px" }} />
-                <a href="/" className="heading-font navbar-brand mr-auto ml-2">
-                    Flight Escrow
-                </a>
+                <div className="mr-auto">
+                    <img src={logo} alt="Airplane" style={{ width: "25px", heigh: "25px" }} />
+                    <Link to="/" className="heading-font navbar-brand ml-2">
+                        Flight Escrow
+                    </Link>
+                    <Link to="/flights" className="mx-4 navbar-links">
+                        Flights
+                    </Link>
+                    <Link to="/add" className="mx-4 navbar-links">
+                        Add
+                    </Link>
+                </div>
+
                 <div className="ml-auto">
                     <span className="mx-1" className="network-name">
                         {networkName}
                     </span>
                     <div className="user-account-details d-inline ml-1">
                         <span className="mx-1">{props.accountBalance} ETH</span>
-                        <span onClick = {handleShow} className="ml-1" className="user-account-address">
+                        <span onClick={handleShow} className="ml-1" className="user-account-address">
                             {props.account.slice(0, 6) + "..." + props.account.slice(-4)}
                         </span>
                     </div>
@@ -44,7 +54,7 @@ export default function Header(props) {
                 <Modal.Header closeButton>
                     <Modal.Title>Account Address</Modal.Title>
                 </Modal.Header>
-    <Modal.Body>{props.account}</Modal.Body>
+                <Modal.Body>{props.account}</Modal.Body>
             </Modal>
         </div>
     );
