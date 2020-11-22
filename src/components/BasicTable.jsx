f9d77b38e7032cac3fc5f77919a9023837049ec8
Parv Garg <parv@somish.com>
2020-11-22 15:01:19 +0530

    added fade

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,54 +1,59 @@
 import React from "react";
+import Fade from "react-bootstrap/Fade";
 import { useTable } from "react-table";
 
 export default function BasicTable(props) {
-    // const columns = useMemo(() => COLUMNS, []);
-    // const data = useMemo(() => props.tableData, [props.tableData]);
+    let loading = true;
     let { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns: props.columns, data: props.data });
+    if (rows.length > 0) {
+        loading = false;
+    } else return null;
     return (
-        <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
-            <thead>
-                {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => (
-                            <th
-                                {...column.getHeaderProps()}
-                                style={{
-                                    borderBottom: "solid 3px red",
-                                    background: "aliceblue",
-                                    color: "black",
-                                    fontWeight: "bold",
-                                }}
-                            >
-                                {column.render("Header")}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map((cell) => {
-                                return (
-                                    <td
-                                        {...cell.getCellProps()}
-                                        style={{
-                                            padding: "10px",
-                                            border: "solid 1px gray",
-                                            background: "papayawhip",
-                                        }}
-                                    >
-                                        {cell.render("Cell")}
-                                    </td>
-                                );
-                            })}
+        <Fade in={!loading}>
+            <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th
+                                    {...column.getHeaderProps()}
+                                    style={{
+                                        borderBottom: "solid 3px red",
+                                        background: "aliceblue",
+                                        color: "black",
+                                        fontWeight: "bold",
+                                    }}
+                                >
+                                    {column.render("Header")}
+                                </th>
+                            ))}
                         </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td
+                                            {...cell.getCellProps()}
+                                            style={{
+                                                padding: "10px",
+                                                border: "solid 1px gray",
+                                                background: "papayawhip",
+                                            }}
+                                        >
+                                            {cell.render("Cell")}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </Fade>
     );
 }
